3c55aaaf6b0d4ab8ef8957082982174db92d014d
steve <steve.hopkins@outlook.com>
2022-12-04 20:41:26 +0000

    Finished Day 4 in Java, but it ain't pretty.

--- 4/Part2.java
+++ 4/Part2.java
@@ -3,7 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.stream.Stream;
 
-public class Part1 {
+public class Part2 {
   static String line;
   static int overlaps;
 
@@ -11,12 +11,16 @@ public class Part1 {
     try (BufferedReader reader = new BufferedReader(new FileReader("4/input.txt"))) {
       while ((line = reader.readLine()) != null) {
         int[] assignments = Stream.of(line.split("[,-]")).mapToInt(Integer::parseInt).toArray();
-        if (assignments[0] >= assignments[2] && assignments[1] <= assignments[3]) overlaps++;
-        else if (assignments[2] >= assignments[0] && assignments[3] <= assignments[1]) overlaps++;
+        if ((assignments[0] >= assignments[2] && assignments[0] <= assignments[3])
+            || assignments[1] >= assignments[2] && assignments[1] <= assignments[3]
+            || (assignments[2] >= assignments[0] && assignments[2] <= assignments[1])
+            || (assignments[3] >= assignments[0] && assignments[3] <= assignments[1])) {
+          overlaps++;
+        }
       }
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
-    System.out.format("Number of ranges fully contained by another range = %d!", overlaps);
+    System.out.format("Number of overlaps = %d!", overlaps);
   }
 }
