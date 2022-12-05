addfa739fa59d95d3781f174bf80680595b6b935
steve <steve.hopkins@outlook.com>
2022-12-05 22:08:45 +0000

    Attempts to make more readable

--- 5/part1.py
+++ 5/part1.py
@@ -1,28 +1,42 @@
 crates = []
 
-with open("input.txt") as file:
-    for line in file:
-        # Select lines that include crates
-        if line.find("[") != -1:
-            num_of_stacks = (len(line)+1)//4
-            for i in range(num_of_stacks):
-                # add empty stacks to crates list
-                if len(crates) < num_of_stacks:
-                    crates.append([])
-            for index, char in enumerate(line, 0):
-                if char.isalpha():
-                    # adds crate to the bottom of its respective stack
-                    crates[(index+1)//4].insert(0, char)
-        # Select lines that include commands
-        if line.startswith("m"):
-            commands = ([int(s) for s in line.split() if s.isdigit()])
-            num_to_move = commands[0]
-            source = crates[commands[1]-1]
-            destination = crates[commands[2]-1]
 
-            # Takes crates off the top of a stack and adds to another
-            for i in range(num_to_move):
-                destination.append(source.pop())
+def main():
+    with open("input.txt") as file:
+        for line in file:
+            # Select only lines that contain crates
+            if line.find("[") != -1:
+                extractCrates(line)
 
-# Print list of the top crate on each stack
-print([stack.pop() for stack in crates if type(stack) == list])
+            # Select only lines that include commands
+            if line.startswith("move"):
+                commands = extractCommands(line)
+                num_to_move = commands[0]
+                source = crates[commands[1]-1]
+                destination = crates[commands[2]-1]
+
+            # Move crates
+                for i in range(num_to_move):
+                    destination.append(source.pop())
+    # Print list of the top crate on each stack
+    print([stack.pop() for stack in crates if type(stack) == list])
+
+
+def extractCrates(line):
+    num_of_stacks = (len(line)+1)//4
+    for i in range(num_of_stacks):
+        # add empty stacks to crates list
+        if len(crates) < num_of_stacks:
+            crates.append([])
+    for index, char in enumerate(line, 0):
+        if char.isalpha():
+            # adds crate to the bottom of its respective stack
+            crates[(index+1)//4].insert(0, char)
+
+
+def extractCommands(line):
+    commands = ([int(s) for s in line.split() if s.isdigit()])
+    return commands
+
+
+main()
